15fccbe9d4b7f665b4349e07d5c0f90fad05ed87
sTranger <853561128@qq.com>
2020-06-12 21:52:42 +0800

    day03

--- evening6-12/goodluck/index/urls.py
+++ evening6-12/goodluck/index/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view)
+    path('', views.index_view),
+    path('prize',views.prize_view)
 ]
--- evening6-12/goodluck/index/views.py
+++ evening6-12/goodluck/index/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
+import random
 from django.shortcuts import render
 
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse('hello,world')
+    if request.method == 'GET':
+        # 存放所有奖品列表
+
+        lis = []
+        for i in range(1, 9):
+            lis.append('/static/images/prize%d.jpg' % i)
+        # 随机列表
+        random.shuffle(lis)
+        return render(request, 'index.html',
+                      {'prizes': lis,
+
+                       })
+
+
+def prize_view(request):
+    return render(request, 'prize.html')
