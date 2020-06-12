99ea8dfbe963f9e2664948fecbbbec5c3cab410c
sTranger <853561128@qq.com>
2020-06-12 15:38:21 +0800

    day03

--- mysite3/news/views.py
+++ mysite3/news/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     # return HttpResponse('<h1>这是newsApp</h1>')
-    return render(request, 'news_index.html')
+    return render(request, 'news/news_index.html')
