78ce48a8c2c283d8242625a3e289361e88b7939d
sTranger <853561128@qq.com>
2020-06-12 11:00:13 +0800

    app

--- mysite3/mysite3/settings.py
+++ mysite3/mysite3/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 
 # tuple 一个元素，后面必须加逗号
 STATICFILES_DIRS = (
