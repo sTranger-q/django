bcb6a0d78a82daefceaa94ebd45aa3ad7c05011f
sTranger <853561128@qq.com>
2020-06-12 17:48:34 +0800

    day03

--- mysite3/bookstore/models.py
+++ mysite3/bookstore/models.py
@@ -6,3 +6,8 @@ class Book(models.Model):
     title = models.CharField('书名', max_length=50, default='')
     price = models.DecimalField('定价', max_digits=7, decimal_places=2,
                                 default=0.0)
+    # 新增字段，必须+default
+    pub = models.CharField('出版社', max_length=120, default='')
+
+    class Meta:
+        db_table = 'book'
