4185c60ef7fb0b8c22ef20b60a6c723b49c4054a
sTranger <853561128@qq.com>
2020-06-13 16:22:23 +0800

    django

--- mysite3/bookstore/models.py
+++ mysite3/bookstore/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
     pub = models.CharField('出版社', max_length=120, default='')
     market_price = models.DecimalField('零售价', max_digits=6, decimal_places=2,
                                        default=0)
+    is_active = models.BooleanField('是否活跃', default=True)
 
     def __str__(self):
         return 'title:%s pub:%s price:%s market_price:%s' % (self.title,
@@ -25,6 +26,7 @@ class Author(models.Model):
     name = models.CharField('作者', max_length=30, default='')
     age = models.IntegerField('年龄', default=1)
     email = models.EmailField('邮箱', null=True)
+    is_active = models.BooleanField('是否活跃', default=True)
 
     class Meta:
         db_table = 'author'
