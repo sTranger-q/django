79d3ef4ba336f0c9f0435d5dc96363afb315aa74
sTranger <853561128@qq.com>
2020-06-15 11:54:28 +0800

    django

--- mysite3/bookstore/admin.py
+++ mysite3/bookstore/admin.py
@@ -6,7 +6,14 @@ from .models import *
 
 class BookManager(admin.ModelAdmin):
     list_display = ['id', 'title', 'pub', 'price', 'market_price']
-    list_display_links = ('title',)
+    # 必须在list_display中
+    list_display_links = ['title']
+
+    list_filter = ['pub']
+    # 根据那些字段查询,模糊查询
+    search_fields = ['title']
+    # 值必须出现在list_display中,不能在list_display_links里
+    list_editable = ['market_price']
 
 
 admin.site.register(Book, BookManager)
