1f1184828bdcc26ab41e87c9708962f6c5c5c097
sTranger <853561128@qq.com>
2020-06-15 11:43:17 +0800

    django

--- mysite3/bookstore/admin.py
+++ mysite3/bookstore/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
-admin.site.register(Book)
+
+class BookManager(admin.ModelAdmin):
+    list_display = ['id', 'title', 'pub', 'price', 'market_price']
+    list_display_links = ('title',)
+
+
+admin.site.register(Book, BookManager)
