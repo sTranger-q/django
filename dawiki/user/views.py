3ced7e47e913f8e1f1c9f97b5df44061837e9d7f
sTranger <853561128@qq.com>
2020-06-16 12:27:19 +0800

    1

--- dawiki/user/views.py
+++ dawiki/user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_view(request):
+    pass
+
+
+def reg_view(request):
+    pass
+
+
+def logout_view(request):
+    pass
