3016649dc3315fb343ba727b79da738105f01ee3
sTranger <853561128@qq.com>
2020-06-24 14:30:56 +0800

    ajax

--- ajax/ajax_mysite1/static/js/common.js
+++ ajax/ajax_mysite1/static/js/common.js
@@ -11,5 +11,11 @@ function createXhr(){
 function getXhr(){
     var xhr=createXhr();
     xhr.open('GET','/test_xhr_get_server',true);
-    
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState==4&&xhr.status==200){
+            var div=document.getElementById('show');
+            div.innerHTML=xhr.responseText;
+        }
+    }
+    xhr.send(null);
 }
